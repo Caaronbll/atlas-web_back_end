c17ab4eb8c1bf2f27e179fba4a8c28b732263f64
Caaronbll <aaronbll46@gmail.com>
2024-01-02 16:27:15 -0600

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""converts a float to a string"""
+
+def to_str(n: float) -> str:
+    """
+    Parameters: a float to convert
+    Returns: a string
+    """
+    return str(n)
