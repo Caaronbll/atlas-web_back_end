7526b1c4c3464cd23e0a61f9e7600c67480c2384
Caaronbll <aaronbll46@gmail.com>
2024-01-04 11:42:32 -0600

    test task 2

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -17,7 +17,8 @@ def measure_time(n: int, max_delay: int) -> float:
 
     end_time = time.time()
 
-    total_time = end_time - start_time
+    #total_time = end_time - start_time
 
-    return total_time/n
+    #return total_time/n
 
+    return (end_time - start_time) / n
