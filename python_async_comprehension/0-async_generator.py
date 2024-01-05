0d54b2edeb9e7bacf3f34aecc3f024340097deec
Caaronbll <aaronbll46@gmail.com>
2024-01-05 14:38:53 -0600

    task 0 comments

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,8 +6,10 @@ import asyncio, random
 
 async def async_generator():
     """
-    Parameters: none
-    Returns: 10 random numbers
+    _summary_
+    loops 10 times
+    waits a second in async
+    returns random # from 1-10
     """
     for _ in range(10):
         await asyncio.sleep(1)
