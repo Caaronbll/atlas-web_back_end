08d7786334fe322184a79681196f88562e010942
Caaronbll <aaronbll46@gmail.com>
2024-01-05 15:06:37 -0600

    task 1 clean up

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 """task 1"""
-import asyncio, random
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Parameters: none
     Returns: random numbers
