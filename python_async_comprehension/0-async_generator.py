0ac97bb45f71ef99b0fe8dbb8525bf3e79e19b8f
Caaronbll <aaronbll46@gmail.com>
2024-01-05 14:36:37 -0600

    cleaning up task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+
 """task 0"""
-
-
 import asyncio, random
 
+
 async def async_generator():
-    """first function"""
+    """
+    Parameters: none
+    Returns: 10 random numbers
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
