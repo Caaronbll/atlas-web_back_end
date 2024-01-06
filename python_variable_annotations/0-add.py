8af34e43a7d611e2e382e2b6e7b2717361d50f01
Caaronbll <aaronbll46@gmail.com>
2024-01-05 20:03:00 -0600

    task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,8 +3,8 @@
 """adds two float integers"""
 
 def add(a: float, b: float) -> float:
-    """
-    Parameters: a and b floats
-    Returns: sum of a and b
-    """
-    return a + b
+  """
+  Parameters: a and b floats
+  Returns: sum of a and b
+  """
+  return a + b
