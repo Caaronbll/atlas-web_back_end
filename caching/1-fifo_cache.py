c4b00c5af9656eafc4674dd60449300cdc94231f
Caaronbll <aaronbll46@gmail.com>
2024-01-09 17:25:39 -0600

    task 1

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """task 1 - making a custon FIFO cache system"""
 
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
@@ -12,26 +12,27 @@ class FIFOCache(BaseCaching):
         """ Initialize the FIFOCache class
         """
         super().__init__()
-        self.cache_data = {}
 
     def put(self, key, item):
         """ Add an item in the cache using FIFO algorithm
         """
-        if key or item is None:
+        if key is None or item is None:
             return
-
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            return
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             # If the cache is full, remove the first item (FIFO)
             first_key = next(iter(self.cache_data))
             del self.cache_data[first_key]
-            print("DISCARD: {}\n".format(first_key))
+            print("DISCARD: {}".format(first_key))
 
         self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key from the cache
         """
-        if key is not None or key not in self.cache_data:
+        if key is None or key not in self.cache_data:
             return None
 
         return self.cache_data[key]
