3d0d99e89e51cbaedcd032ac29859afba0f7e7de
Caaronbll <aaronbll46@gmail.com>
2024-01-16 16:38:14 -0600

    task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,33 @@ Task 0 - Regex-ing
 """
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        Original_message = record.getMessage()
+        New_message = filter_datum(
+            self.fields,
+            self.REDACTION,
+            Original_message,
+            self.SEPARATOR
+        )
+        record.msg = New_message
+        return super().format(record)
+
+
 
 
 def filter_datum(fields: List[str], redaction: str,
