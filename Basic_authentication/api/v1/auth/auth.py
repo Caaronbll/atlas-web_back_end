432f950f2ef906147f2f4929746efb877b88d598
Caaronbll <aaronbll46@gmail.com>
2024-01-22 13:20:45 -0600

    task 3

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Task 3 - Auth class
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """ Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Placeholder method for checking authentication requirement.
+
+        Args:
+        - path (str): The path to check for authentication.
+        - excluded_paths (List[str]): List of paths to be excluded.
+
+        Returns:
+        - bool: Always returns False for demonstration purposes.
+        """
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Placeholder method for extracting authorization header.
+
+        Args:
+        - request: The Flask request object (default=None).
+
+        Returns:
+        - str: Always returns None for demonstration purposes.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Placeholder method for retrieving the current user.
+
+        Args:
+        - request: The Flask request object (default=None).
+
+        Returns:
+        - TypeVar('User'): Always returns None for demonstration purposes.
+        """
+        return None
