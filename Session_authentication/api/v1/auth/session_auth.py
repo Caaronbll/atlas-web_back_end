cdd6cb6c36d6a6468d8dec381be7dad1bb85d37a
Caaronbll <aaronbll46@gmail.com>
2024-01-23 15:53:15 -0600

    task 6

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -24,3 +24,15 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Returns a User based on cookie value """
+        cookie_value = self.session_cookie(request)
+        if cookie_value is None:
+            return None
+        user_id = self.user_id_for_session_id(cookie_value)
+        if user_id is None:
+            return None
+
+        user_instance = User.get(user_id)
+        return user_instance
