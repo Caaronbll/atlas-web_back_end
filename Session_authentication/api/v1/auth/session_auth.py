a3f6e101319f9297fe0af27ebbbc5a0e109f3922
Caaronbll <aaronbll46@gmail.com>
2024-01-23 14:07:05 -0600

    task 2

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,7 +3,17 @@
 """
 
 from api.v1.auth.auth import Auth
-
+import uuid
 
 class SessionAuth(Auth):
     """ SessionAuth class that inherits from Auth"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a new session ID for a user """
+        if user_id is None or type(user_id) is not str:
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
