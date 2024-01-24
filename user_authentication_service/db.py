651b0ff23ba9ec62a6fac67ea40636cbd95760c4
Caaronbll <aaronbll46@gmail.com>
2024-01-24 14:15:05 -0600

    task 1

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,8 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
 from user import Base, User
+from sqlalchemy.exc import IntegrityError
 
 
 class DB:
@@ -34,6 +34,11 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Adds a new user to the database """
         new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
-        return new_user
+        try:
+            self._session.add(new_user)
+            self._session.commit()
+            return new_user
+        except IntegrityError:
+            # Handle IntegrityError, for example, if the email is not unique
+            self._session.rollback()
+            return None
