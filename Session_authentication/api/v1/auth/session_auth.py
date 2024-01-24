1032bdf4e7ae9054ce46e180e57e7ec506afbd76
Caaronbll <aaronbll46@gmail.com>
2024-01-24 13:31:32 -0600

    task 6

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -7,8 +7,6 @@ from models.user import User
 from flask import Flask, request, jsonify, make_response
 from api.v1.views import app_views
 
-app = Flask(__name__)
-
 
 class SessionAuth(Auth):
     """ Session class for authentication """
@@ -39,31 +37,3 @@ class SessionAuth(Auth):
                 return User.get(user_id)
 
         return None
-
-    @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-    def auth_session_login():
-        """ Handles authentication for Session """
-        email = request.form.get('email')
-        password = request.form.get('password')
-
-        if not email:
-            return jsonify({"error": "email missing"}), 400
-        if not password:
-            return jsonify({"error": "password missing"}), 400
-
-        user = User.search({'email': email})
-
-        if not user:
-            return jsonify({"error": "no user found for this email"}), 404
-
-        if not user[0].is_valid_password(password):
-            return jsonify({"error": "wrong password"}), 401
-
-        from api.v1.app import auth
-        session_id = auth.create_session(user[0].id)
-        user_dict = user[0].to_json()
-
-        response = make_response(jsonify(user_dict))
-        response.set_cookie(app.config.get("SESSION_NAME", "_my_session_id"), session_id)
-
-        return response
