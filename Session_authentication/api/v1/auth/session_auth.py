365b2c3d257ac4e2dd060f671b64432c34995705
Caaronbll <aaronbll46@gmail.com>
2024-01-24 13:22:16 -0600

    task 6

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 """
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -24,3 +25,13 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Returns user instance based on cookie value"""
+        session_id = self.session_cookie(request)
+        if session_id:
+            user_id = self.user_id_for_session_id(session_id)
+            if user_id:
+                return User.get(user_id)
+
+        return None
