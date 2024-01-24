6a455cc491467cfa91490e9a4263f2f17398aaae
Caaronbll <aaronbll46@gmail.com>
2024-01-24 13:12:57 -0600

    task 5

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -60,6 +60,8 @@ def before_request() -> str:
         return
     if auth.authorization_header(request) is None:
         abort(401)
+    if auth.session_cookie(request) is None:
+        abort(401)
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
