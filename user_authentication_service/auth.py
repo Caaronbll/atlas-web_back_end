9860bb28c03587fd2881fff8d717bab6afd2755c
Caaronbll <aaronbll46@gmail.com>
2024-01-25 17:35:34 -0600

    task 10

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -47,3 +47,10 @@ class Auth(DB):
                 return True
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """ Retruns session ID based on email """
+        user = self._db.find_user_by(email=email)
+        user.session_id = _generate_uuid()
+        self._db.update_user(user.session_id)
+        return user.session_id
