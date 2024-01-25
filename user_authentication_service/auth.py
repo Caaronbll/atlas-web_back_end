2910c1df5d9e177a3e0e0b11a7bf4be1dcd7c751
Caaronbll <aaronbll46@gmail.com>
2024-01-25 17:56:49 -0600

    task 12

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -53,3 +53,14 @@ class Auth(DB):
         user = self._db.find_user_by(email=email)
         user.session_id = _generate_uuid()
         return user.session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ Returns user from session ID """
+        if session_id is None:
+            return None
+
+        user = self._db.find_user_by(session_id)
+        if user is None:
+            return None
+
+        return user
