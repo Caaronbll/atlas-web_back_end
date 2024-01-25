8a08d4bac23af1c083df2909cb6a0e9b3dc1242e
Caaronbll <aaronbll46@gmail.com>
2024-01-25 17:01:12 -0600

    task 6

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,32 +1,16 @@
 #!/usr/bin/env python3
 """
-Task 6 - Basic Flask App
+Task 6 - Basic Flask app
 """
-from flask import Flask, jsonify, request, abort, redirect
-from auth import Auth
-
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-AUTH = Auth()
 
 
-@app.route('/', methods=['GET'], strict_slaches=False)
-def welcome() -> str:
-    """ returns message from api"""
+@app.route('/', methods=['GET'])
+def welcome():
+    """ returns a message """
     return jsonify({"message": "Bienvenue"})
-
-
-@app.route('/users', methods=['POST'], strict_slaches=False)
-def register_users(email: str, password: str):
-    """Endpoint to register a user"""
-    email = request.form.get('email')
-    password = request.form.get('password')
-    try:
-        Auth.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"})
-    except ValueError:
-        return jsonify({"message": "email already registered"})
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
