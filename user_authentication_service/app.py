a300c23c1852b28694b2fdc34655b87a10c56d73
Caaronbll <aaronbll46@gmail.com>
2024-01-25 17:44:10 -0600

    task 11

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Task 6 - Basic Flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 
@@ -28,6 +28,26 @@ def register_user():
 
     except Auth.UserAlreadyExistsError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """ Login to the session """
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        if not AUTH.valid_login(email, password):
+            abort(401)
+
+        session_id = AUTH.create_session(email)
+
+        response = make_response(jsonify({"email": email, "message": "logged in"}))
+        response.set_cookies("session_id", session_id)
+        return response
+
+    except Auth.UserNotFoundError:
+        abort(401)
 
 
 if __name__ == "__main__":
