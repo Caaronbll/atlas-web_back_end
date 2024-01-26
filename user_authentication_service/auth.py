7531e126a49c4d1115106413dae9fa0af6f14da0
Caaronbll <aaronbll46@gmail.com>
2024-01-25 18:01:18 -0600

    task 12

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -60,7 +60,7 @@ class Auth(DB):
             return None
 
         user = self._db.find_user_by(session_id=session_id)
-        if user is None:
+        if user:
+            return user
+        else:
             return None
-
-        return user
