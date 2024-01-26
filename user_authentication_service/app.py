cc6802cd42d8e0e9afabd47e54d5e64192647f32
Caaronbll <aaronbll46@gmail.com>
2024-01-25 18:43:20 -0600

    task 17

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -72,5 +72,16 @@ def profile():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """ Generates token from email """
+    try:
+        email = request.form.get("email")
+        reset_token = request.form.get("reset_token")
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
