26825edb3b560cc6c580c509fbf2ed1e948df5be
Caaronbll <aaronbll46@gmail.com>
2024-01-25 18:37:53 -0600

    task 16 - fixing this error

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -76,10 +76,10 @@ class Auth(DB):
 
     def get_reset_password_token(self, email: str) -> str:
         """ Returns the reset password token """
-        user = self._db.find_user_by(email=email)
-        if user is None:
-            raise NoResultFound
-        elif user:
+        try:
+            user = self._db.find_user_by(email=email)
             new_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=new_token)
-        return new_token
+            return new_token
+        except NoResultFound:
+            raise ValueError
