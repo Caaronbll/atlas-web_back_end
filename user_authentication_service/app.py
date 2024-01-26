0564503f0a7145c6f04144686b62f1b4e28ad9a0
Caaronbll <aaronbll46@gmail.com>
2024-01-25 18:19:36 -0600

    task 14 - changed 404 to 403

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -55,7 +55,7 @@ def logout():
 
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
-        abort(404)
+        abort(403)
 
     AUTH.destroy_session(user.id)
     return redirect("/")
