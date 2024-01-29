3b6d28943aaf59872044f48b17d2fd727dab6b07
Caaronbll <aaronbll46@gmail.com>
2024-01-29 17:19:41 -0600

    task 3

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -43,3 +43,23 @@ class TestGetJson(unittest.TestCase):
         with mock.patch('requests.get', return_value=mock.Mock(
                 json=lambda: test_payload)):
             self.assertEqual(get_json(testUrl), test_payload)
+
+class TestMemoize(unittest.TestCase):
+    """ Tests memoize function """
+
+    def test_memoize(self):
+        """ Testing get_json """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with mock.patch.object(TestClass, 'a_method') as fn:
+            test_class = TestClass()
+            test_class.a_property
+            test_class.a_property
+            fn.assert_called_once()
