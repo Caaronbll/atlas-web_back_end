b1945f5e71883313ca1114078359122658fd36a6
Caaronbll <aaronbll46@gmail.com>
2024-01-29 19:18:16 -0600

    task 6

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -32,3 +32,19 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_org.return_value = {"repos_url": "http://testurl.com"}
             client = GithubOrgClient("test")
             self.assertEqual(client._public_repos_url, "http://testurl.com")
+
+    @patch('client.get_json')
+    def test_public_repos(self, test_json):
+        """ Testing public repo results """
+        test_json.return_value = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+        ]
+        with patch.object(GithubOrgClient, '_public_repos_url',
+                          filler=PropertyMock) as mk:
+            mk.return_value = "www.test.com"
+            test = GithubOrgClient("test")
+            result = test.public_repos()
+            self.assertEqual(result, ["repo1", "repo2"])
+            mk.assert_called_once()
+            test_json.assert_called_once()
