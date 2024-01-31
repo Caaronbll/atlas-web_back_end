497c44dd3dd8f007331e9c2e8b709c57c9511a14
Caaronbll <aaronbll46@gmail.com>
2024-01-31 11:40:22 -0600

    task 0 - changed path

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -4,14 +4,16 @@ Task 0
 """
 from flask import Flask, render_template
 from flask_babel import Babel
+
+
 app = Flask(__name__)
 babel = Babel(app)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
     """ Routeds to 0-index """
-    return render_template('templates/0-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
