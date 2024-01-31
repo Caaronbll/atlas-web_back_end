59cf31fdf1d3cf25de0c503f9750c1e00be1f097
Caaronbll <aaronbll46@gmail.com>
2024-01-31 13:54:47 -0600

    task 2

---
+++ i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Task 1
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config():
+    """ Configuration class """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config())
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """ Routeds to 1-index """
+    return render_template('1-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ Returns the best language match """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
