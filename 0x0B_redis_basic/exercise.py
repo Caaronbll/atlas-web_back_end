d52b7f7197baf8c673ff6fa6335ba038a011afaa
Caaronbll <aaronbll46@gmail.com>
2024-02-05 16:09:26 -0600

    task 2

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,6 +4,17 @@
 import redis
 import uuid
 from typing import Union, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """ Decorator for counting function calls """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper call method """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache():
@@ -14,6 +25,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Store data in the Cache """
         key = str(uuid.uuid4())
