c5e977940b7fb51946d00b0daf14f0064595f9fa
Caaronbll <aaronbll46@gmail.com>
2024-02-07 01:26:40 -0600

    task 8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Task 8 """
+
+import pymongo
+
+def list_all(mongo_collection):
+    """ lists all documents in a collection """
+
+    if mongo_collection:
+        return list(mongo_collection.find())
+    return []
